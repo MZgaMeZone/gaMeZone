b7304978102bcdadc55fc8a84273ef28d79cb56f
gomao <gboycdw@gmail.com>
2023-05-12 00:27:10 +0900

    [update] 미사용 코드 주석처리

--- BackEnd/src/services/index.js
+++ BackEnd/src/services/index.js
@@ -1,4 +1,4 @@
-export * from "./order-service.js";
+// export * from "./order-service.js";
 export * from "./user-service.js";
 // export * from './product-services.js';
 // export * from './category-services.js';
