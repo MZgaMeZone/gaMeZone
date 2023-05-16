bbb7a4eecd561c7c6abd608fa6d29308269b1539
gomao <gboycdw@gmail.com>
2023-05-16 20:55:25 +0900

    [add] 루트 페이지, 메인 페이지 설정

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 import logo from "./logo.svg";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TimeStopGame from "./components/Games/StopWatch/timeStop";
 import GameStarter from "./components/gameStart";
 import Admin from "./pages/adminPage/admin";
 import Community from "./pages/communityPage/community";
-
+import Main from "./pages/mainPage/main";
+import Rootpage from "./pages/rootPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route  path="/" element={ <TimeStopGame />} />
-        <Route  path="/admin" element={ <Admin />} />
-        <Route  path="/community" element={ <Community />} />
+        <Route path="/" element={<Rootpage />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/game1" element={<TimeStopGame />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
     </BrowserRouter>
   );
