b69934c00ea6fb5681860c672dd21c33ffbdee61
Minyoung Lee <zhal7779@gmail.com>
2023-05-16 21:48:09 +0900

    [FIX] git conflict resolved

--- FrontEnd/src/pages/adminPage/admin.tsx
+++ FrontEnd/src/pages/adminPage/admin.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./admin.module.css";
 const Admin = () =>{
   return(
-   <>
+  <>
     <div className={styles.container}>
       <div className={styles.header}><p>ADMIN - 메인 대시보드</p></div>
       <div className={styles.sub_menu}>
@@ -10,7 +10,7 @@ const Admin = () =>{
         <button>일자별 요약</button>
       </div>
     </div>
-   </>
+  </>
   )
 }
 export default Admin
