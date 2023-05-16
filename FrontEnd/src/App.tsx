0ae0730852761a0bedbb9d31cdc80f061ed09e4d
bible <mongry999@gmail.com>
2023-05-16 19:41:37 +0900

    [add] game loading page in app.tsx

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,28 +1,25 @@
 import * as React from "react";
 import logo from "./logo.svg";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TimeStopGame from "./components/Games/StopWatch/timeStop";
 import GameStarter from "./components/gameStart";
 import Admin from "./pages/adminPage/admin";
 import Community from "./pages/communityPage/community";
-
+import GameLoading from "./pages/gamePages/gameLoading";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route  path="/" element={ <TimeStopGame />} />
-        <Route  path="/admin" element={ <Admin />} />
-        <Route  path="/community" element={ <Community />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<TimeStopGame />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/community" element={<Community />} />
+                <Route path="/gameLoading" element={<GameLoading />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ FrontEnd/src/pages/gamePages/gameLoading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GameLoading = () => {
+    return <div> 안뇽 </div>;
+};
+
+export default GameLoading;
