183e66c0d0d4c459ccae8b097ada7ab4deac7152
bible <mongry999@gmail.com>
2023-05-16 23:30:23 +0900

    [add] gameLoading.module.css

--- FrontEnd/src/pages/gamePages/gameLoading.tsx
+++ FrontEnd/src/pages/gamePages/gameLoading.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
+import "../../style/gameLoading.module.css";
 
 const GameLoading = () => {
     return (
-        <Container>
-            <Header>
+        <div className="container">
+            <div className="header">
                 <p>로딩 중</p>
                 <button></button>
-            </Header>
+            </div>
             <body>
                 <div>
                     <div>
@@ -37,26 +38,8 @@ const GameLoading = () => {
                     </div>
                 </div>
             </body>
-        </Container>
+        </div>
     );
 };
 
 export default GameLoading;
-
-const Container = styled.div`
-    box-sizing: border-box;
-
-    width: 35rem;
-    height: 20rem;
-    margin: 10rem auto auto auto;
-    background: #c0c0c0;
-    border: 1px solid #000000;
-    box-shadow: 3px 3px 4px #1c1c1c;
-`;
-
-const Header = styled.div`
-    width: 34rem;
-    height: 2rem;
-    display
-    background: #000080;
-`;
