ea17cce16c851cc22f97172ea09093aae4307fbd
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-05-16 21:34:33 +0900

    [STYLE] reset.css, style.css파일 추가

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,25 +1,22 @@
 import * as React from "react";
 import logo from "./logo.svg";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TimeStopGame from "./components/Games/StopWatch/timeStop";
 import GameStarter from "./components/gameStart";
 import Admin from "./pages/adminPage/admin";
 import Community from "./pages/communityPage/community";
-
+import MyPage from "./pages/myPage/mypage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route  path="/" element={ <TimeStopGame />} />
-        <Route  path="/admin" element={ <Admin />} />
-        <Route  path="/community" element={ <Community />} />
+        <Route path="/" element={<TimeStopGame />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
