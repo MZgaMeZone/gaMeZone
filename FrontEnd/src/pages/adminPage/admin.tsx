df2f818c2ae2ebc1aa5ebb507ff9871611a52a53
Minyoung Lee <zhal7779@gmail.com>
2023-05-16 21:38:58 +0900

    [add] 반응형 추가

--- FrontEnd/src/pages/adminPage/admin.tsx
+++ FrontEnd/src/pages/adminPage/admin.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./admin.module.css";
 const Admin = () =>{
   return(
-    <h1>관리자 페이지 입니다.</h1>
+   <>
+    <div className={styles.container}>
+      <div className={styles.header}><p>ADMIN - 메인 대시보드</p></div>
+      <div className={styles.sub_menu}>
+        <button>방문자 현황</button>
+        <button>일자별 요약</button>
+      </div>
+    </div>
+   </>
   )
 }
 export default Admin
