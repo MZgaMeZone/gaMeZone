84a8b4bd3408ff75f114a1ad4635f8afab929e60
Minyoung Lee <zhal7779@gmail.com>
2023-05-17 18:23:30 +0900

    [add] 메뉴 아이콘 추가, 클릭시 액티브 유지

--- FrontEnd/src/pages/adminPage/menu.tsx
+++ FrontEnd/src/pages/adminPage/menu.tsx
@@ -1,42 +1,55 @@
-import React,{useState,useEffect} from "react";
-import styles from "../../../src/style/adminMenu.module.css";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import styles from '../../../src/style/adminMenu.module.css';
+import { useNavigate } from 'react-router-dom';
 
 type MenuIdx = {
   idx: number;
 };
 
-const Menu = ({idx}:MenuIdx) =>{
-  const navigate = useNavigate()
-  const [menu,setMenu] = useState<number>(idx);
-  const handleClick=(idx:number) =>{
-    setMenu(idx)
-  }
-  console.log(menu)
-  useEffect(()=>{
-    if(menu === 0) {
-      navigate('/admin')
-    } else if(menu === 1){
-      navigate('/admin/information')
-    } else if(menu === 2){
-      navigate('/admin/record')
-    } else if(menu === 3){
-      navigate('/admin/user')
-    } else if(menu === 4){
-      navigate('/')
+const Menu = ({ idx }: MenuIdx) => {
+  const navigate = useNavigate();
+  const [menu, setMenu] = useState<number>(idx);
+  const handleClick = (idx: number) => {
+    setMenu(idx);
+  };
+  useEffect(() => {
+    if (menu === 0) {
+      navigate('/admin');
+    } else if (menu === 1) {
+      navigate('/admin/information');
+    } else if (menu === 2) {
+      navigate('/admin/record');
+    } else if (menu === 3) {
+      navigate('/admin/user');
+    } else if (menu === 4) {
+      navigate('/');
     }
-
-  },[menu])
-  return(
-  <div className={styles.wrapper}>
-    <div className={styles.menu}>
-        <div onClick={()=>handleClick(0)}><p>대시보드</p></div>
-        <div onClick={()=>handleClick(1)}><p>게임 정보</p></div>
-        <div onClick={()=>handleClick(2)}><p>게임 기록</p></div>
-        <div onClick={()=>handleClick(3)}><p>회원 정보</p></div>
-        <div onClick={()=>handleClick(4)}><p>홈페이지 바로가기</p></div>
+  }, [menu]);
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.menu}>
+        <div className={styles.menu_dashboard} onClick={() => handleClick(0)}>
+          {menu === 0 ? <div className={styles.selected_menu}></div> : null}
+          <p>대시보드</p>
+        </div>
+        <div className={styles.menu_info} onClick={() => handleClick(1)}>
+          {menu === 1 ? <div className={styles.selected_menu}></div> : null}
+          <p>게임 정보</p>
+        </div>
+        <div className={styles.menu_record} onClick={() => handleClick(2)}>
+          {menu === 2 ? <div className={styles.selected_menu}></div> : null}
+          <p>게임 기록</p>
+        </div>
+        <div className={styles.menu_user} onClick={() => handleClick(3)}>
+          {menu === 3 ? <div className={styles.selected_menu}></div> : null}
+          <p>회원 정보</p>
+        </div>
+        <div className={styles.menu_home} onClick={() => handleClick(4)}>
+          {menu === 4 ? <div className={styles.selected_menu}></div> : null}
+          <p>홈페이지 바로가기</p>
+        </div>
+      </div>
     </div>
-  </div>
-  )
-}
-export default Menu
+  );
+};
+export default Menu;
