8d62183f9dff2c1dc89c6257c935279ba051da9a
gomao <gboycdw@gmail.com>
2023-05-17 12:06:16 +0900

    [modified] main/header/footer 컴포넌트 분리

--- FrontEnd/src/pages/mainPage/main-footer.tsx
+++ FrontEnd/src/pages/mainPage/main-footer.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
-const MainFooter = () => {
-  //   현재 시간을 출력하는 함수
+
+const MainFooter = (props: any) => {
+  const mainModal = props.mainModal;
+  const setMainModal = props.setMainModal;
   const [timer, setTimer] = React.useState("");
+
+  //   현재 시간을 출력하는 함수
   React.useEffect(() => {
     const updateTimer = () => {
       const now = new Date();
@@ -26,7 +30,7 @@ const MainFooter = () => {
     <FooterBar>
       <StartButton
         onClick={() => {
-          alert("ㅎㅇ");
+          setMainModal(!mainModal);
         }}
       >
         카테고리
@@ -45,7 +49,7 @@ const FooterBar = styled.div`
   display: flex;
   margin-bottom: auto;
   align-items: center;
-  height: 4rem;
+  height: 5rem;
   position: sticky;
   bottom: 0;
   z-index: 2;
@@ -62,12 +66,12 @@ const StartButton = styled.button`
 `;
 
 const SubButton = styled.button`
-  margin: 0.4rem;
+  margin: 1rem;
   border-radius: 0;
   font-size: 2rem;
   border: 1px none;
   width: 25rem;
-  height: 2.5rem;
+  height: 3.5rem;
   box-shadow: 1px 1px 1px rgb(55, 55, 55);
 `;
 
@@ -76,13 +80,13 @@ const Clock = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 10rem;
   height: 3.5rem;
   font-size: 2rem;
   margin-left: auto;
   margin-right: 2rem;
   padding: 0.5rem;
-  height: 2rem;
-  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.2);
+  width: 12rem;
+  height: 3.8rem;
+  box-shadow: inset 0 2px 6px rgba(0, 0, 0, 0.5);
   background-color: #e9e9e9;
 `;
