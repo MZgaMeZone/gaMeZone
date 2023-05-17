df95daec55d018b8ce1eda4c38ece7281751371c
bible <mongry999@gmail.com>
2023-05-18 08:06:30 +0900

    [modified]ranking componenent css

--- FrontEnd/src/pages/gamePages/gameRanking.tsx
+++ FrontEnd/src/pages/gamePages/gameRanking.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import "../../style/gameRanking.css";
 import exitImg from "../../style/icons/x-solid.svg";
+import backgroundImg from "../../style/icons/ranking-background.png";
 import rankingFavicon from "../../style/icons/ranking_favicon.svg";
 
 import { Link } from "react-router-dom";
@@ -25,7 +26,66 @@ const GameRanking = (props: { setShowranking: (show: boolean) => void }) => {
           <img src={exitImg} alt="exitImg" />
         </div>
       </div>
-      <div className="ranking-container-body"></div>
+      <div
+        className="ranking-container-body"
+        style={{ backgroundImage: `url(${backgroundImg})` }}
+      >
+        <div className="ranking-container-body-title">Ranking Zone</div>
+        <div className="ranking-list">
+          <ol>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">angomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">gomao</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+            <li>
+              <span className="ranking-list-idx">1.</span>
+              <p className="ranking-list-userID">o</p>
+              <p className="ranking-list-score">000점</p>
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
