39f1fdbe3e55f1c2ca1a3c00563a1bde19ea5640
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-18 22:55:38 +0900

    [add] post router 구현

--- BackEnd/src/routers/index.js
+++ BackEnd/src/routers/index.js
@@ -1,4 +1,3 @@
 export * from "./order-routers.js";
 export * from "./user-routers.js";
-// export * from "./product-routers.js";
-// export * from "./category-routers.js";
+export * from "./post-routers.js";
