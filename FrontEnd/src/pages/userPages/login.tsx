c72c8b41dc44807d6137a9dc8a0af1db406f3130
minu <wlwhsxz@gmail.com>
2023-05-19 00:25:18 +0900

    Feat: 로그인 CSS 추가

--- FrontEnd/src/pages/userPages/login.tsx
+++ FrontEnd/src/pages/userPages/login.tsx
@@ -14,6 +14,10 @@ function Login() {
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    if (email === "" || password === "") {
+      console.log("이메일 비밀번호 빈칸");
+      return alert("이메일과 비밀번호 모두 입력해주세요.");
+    }
     axios
       .post("http://localhost:8080/api/users/login", {
         email,
@@ -28,6 +32,7 @@ function Login() {
       })
       .catch((e) => {
         console.log(e);
+        alert("올바른 이메일과 비밀번호를 입력하세요.");
       });
   }
 
@@ -41,29 +46,38 @@ function Login() {
         minWidth: "900px",
       }}
     >
-      <h1>로그인 페이지입니다.</h1>
-      <LoginForm onSubmit={handleSubmit}>
-        이메일{" "}
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        비밀번호
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <LoginButton>로그인</LoginButton>
-        <SignupButton onClick={() => navigate("/signup")}>
-          회원가입
-        </SignupButton>
-      </LoginForm>
+      <LoginSection>
+        <LoginContainer>
+          <LoginHeader>로그인</LoginHeader>
+
+          <LoginForm onSubmit={handleSubmit}>
+            <EmailForm>
+              <p>이메일</p>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </EmailForm>
+            <PasswordForm>
+              비밀번호
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </PasswordForm>
+            <LoginButton type="submit">로그인</LoginButton>
+            <SignupButton onClick={() => navigate("/signup")}>
+              회원가입
+            </SignupButton>
+          </LoginForm>
+        </LoginContainer>
+      </LoginSection>
       <MainBody mainModal={mainModal} setMainModal={setMainModal}></MainBody>
       <MainFooter
         mainModal={mainModal}
@@ -74,6 +88,67 @@ function Login() {
 }
 export default Login;
 
-const LoginForm = styled.form``;
-const LoginButton = styled.button``;
-const SignupButton = styled.button``;
+const LoginSection = styled.div`
+  background-color: var(--background--gray);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--background--gray);
+  border: 1px solid #000000;
+  box-shadow: 3px 3px 4px #1c1c1c;
+  width: 128rem;
+  height: 72rem;
+  overflow: auto;
+`;
+
+const LoginContainer = styled.div`
+  background-color: var(--background--gray);
+  padding: 0.5rem 0;
+`;
+
+const LoginHeader = styled.div`
+  margin: 1rem;
+  padding: 1rem;
+  background-color: var(--color--header);
+  color: white;
+  font-size: 2rem;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 12rem;
+  padding: 7rem;
+`;
+
+const EmailForm = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  font-size: 3.5rem;
+  input {
+    font-size: 3rem;
+  }
+  margin: 1.5rem 0;
+`;
+
+const PasswordForm = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  font-size: 3.5rem;
+  input {
+    font-size: 2.5rem;
+  }
+  margin: 1.5rem 0;
+`;
+const LoginButton = styled.button`
+  font-size: 2.5rem;
+  margin: 1rem 0;
+  border: 1px solid black;
+  margin: 3rem 0 1.5rem;
+`;
+const SignupButton = styled.button`
+  font-size: 2.5rem;
+  margin: 1.5rem 0;
+  border: 1px solid black;
+`;
