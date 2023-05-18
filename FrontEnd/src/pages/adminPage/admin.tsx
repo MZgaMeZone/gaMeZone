e53d4977d48d2d99a0f9a0531cee1b3c3a4f3319
Minyoung Lee <zhal7779@gmail.com>
2023-05-18 23:13:08 +0900

    [fix] 관리자페이지 css 오류 수정

--- FrontEnd/src/pages/adminPage/admin.tsx
+++ FrontEnd/src/pages/adminPage/admin.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from '../../../src/style/admin.module.css';
+import styles from '../../style/admin.module.css';
+import styled from 'styled-components';
 import Menu from './menu';
 import Footer from './footer';
 import { ReactComponent as Mushiroom } from '../../style/icons/mushroom.svg';
@@ -10,26 +11,32 @@ const Admin = () => {
       <div className={styles.container}>
         <header>
           <div>
+            <p className={styles.title}>대시보드</p>
             <Mushiroom style={{ width: '11.5rem', height: '5.6rem' }} />
           </div>
-          <p>대시보드</p>
         </header>
         <nav>
-          <button>
-            <p>방문자 현황</p>
-          </button>
-          <button>
-            <p>일자별 요약</p>
-          </button>
-          <button>
-            <p>통계</p>
-          </button>
+          <div>
+            <button>
+              <p>방문자 현황</p>
+            </button>
+            <button>
+              <p>일자별 요약</p>
+            </button>
+            <button>
+              <p>통계</p>
+            </button>
+          </div>
         </nav>
-        <main>
-          <div className={styles.content}></div>
-        </main>
+        <ScrollCotent>
+          <main>
+            <div className={styles.content}></div>
+          </main>
+        </ScrollCotent>
         <footer>
-          <div></div>
+          <div className={styles.footer_box}>
+            <div className={styles.footer_content}></div>
+          </div>
         </footer>
       </div>
       <Footer idx={0} />
@@ -37,3 +44,9 @@ const Admin = () => {
   );
 };
 export default Admin;
+
+const ScrollCotent = styled.div`
+  overflow-y: auto;
+
+  height: calc(100vh- 5rem);
+`;
