a2251b2de22253e0218b699832934222eccee2ba
bible <mongry999@gmail.com>
2023-05-21 20:17:16 +0900

    [modified] 랭킹 get요청 url 파라미터 수정

--- FrontEnd/src/pages/gamePages/gameRanking.tsx
+++ FrontEnd/src/pages/gamePages/gameRanking.tsx
@@ -27,9 +27,7 @@ const GameRanking = (props: {
 
   useEffect(() => {
     axios
-      .get(
-        `${process.env.REACT_APP_API_URL}/api/scores/${gameId}/averageScore?num=10`
-      )
+      .get(`${process.env.REACT_APP_API_URL}/api/scores/${gameId}/avr?num=10`)
       .then((res) => {
         setRankingData(res.data);
         console.log(res.data);
