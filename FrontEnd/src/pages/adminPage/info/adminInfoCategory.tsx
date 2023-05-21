9f73bb56f6f78322a4edd3d1190d738465c47184
Minyoung Lee <zhal7779@gmail.com>
2023-05-22 00:08:10 +0900

    [add] 관리자페이지 - 카테고리 수정 동작구현

--- FrontEnd/src/pages/adminPage/info/adminInfoCategory.tsx
+++ FrontEnd/src/pages/adminPage/info/adminInfoCategory.tsx
@@ -5,11 +5,35 @@ interface Category {
   _id: string;
   categoryName: string;
 }
+interface Input {
+  editInput: string;
+  addInput: string;
+}
 const AdminInfoCategory = () => {
   const URL: string = 'http://localhost:8080/api/categories';
 
   const [data, setData] = useState<Category[]>([]);
-  const [inputValue, setInputValue] = useState<string>('');
+
+  // input 처리
+  const [inputs, setInputs] = useState<Input>({
+    addInput: '',
+    editInput: '',
+  });
+
+  const { addInput, editInput } = inputs;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+    const { value, name } = e.target;
+    if (name === 'editInput') {
+      setEditCategoryName(value);
+    } else {
+      setInputs((prevInputs) => ({
+        ...prevInputs,
+        [name]: value,
+      }));
+    }
+  };
+
   useEffect(() => {
     axios
       .get(URL)
@@ -19,28 +43,47 @@ const AdminInfoCategory = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
+  //추가
 
   const addHandleClick = async () => {
+    if (addInput === '') {
+      alert('카테고리를 입력해주세요.');
+    }
     try {
       const res = await axios.post(URL, {
-        categoryName: inputValue,
+        categoryName: addInput,
       });
       console.log(res.data);
       setData([...data, res.data]);
-      setInputValue('');
+      setInputs((prev) => ({
+        ...prev,
+        addInput: '',
+      }));
     } catch (err) {
       console.error(err);
     }
   };
 
+  // 수정
+
+  const [editCategoryName, setEditCategoryName] = useState('');
+  const [isEditing, setIsEditing] = useState<string | null>(null);
+  const handleEditClick = (id: string) => {
+    const category = data.find((item) => item._id === id);
+    if (category) {
+      setIsEditing(id);
+      setEditCategoryName(category.categoryName);
+    }
+  };
+
+  const handleSaveClick = (id: string) => {
+    setIsEditing(null);
+  };
+
+  //삭제
   const deleteHandleClick = async (id: string) => {
     try {
-      const res = await axios.delete(
-        `http://localhost:8080/api/categories/${id}`
-      );
+      const res = await axios.delete(URL + `/${id}`);
       console.log(res.data);
       setData(data.filter((item) => item._id !== id));
     } catch (err) {
@@ -55,8 +98,9 @@ const AdminInfoCategory = () => {
         <Input
           type="text"
           placeholder="카테고리를 입력해주세요."
-          value={inputValue}
-          onChange={handleChange}
+          value={addInput}
+          name="addInput"
+          onChange={(e) => handleChange(e, '')}
         />
         <Button onClick={() => addHandleClick()}>추가</Button>
       </Container>
@@ -64,9 +108,22 @@ const AdminInfoCategory = () => {
         <Title>카테고리 내역</Title>
         {data.map((item: Category) => (
           <EditContent key={item._id}>
-            <p>{item.categoryName}</p>
+            {isEditing === item._id ? (
+              <input
+                value={editCategoryName}
+                name="editInput"
+                onChange={(e) => handleChange(e, item._id)}
+                autoFocus
+              />
+            ) : (
+              <p>{item.categoryName}</p>
+            )}
             <div>
-              <Button>수정</Button>
+              {isEditing === item._id ? (
+                <Button onClick={() => handleSaveClick(item._id)}>저장</Button>
+              ) : (
+                <Button onClick={() => handleEditClick(item._id)}>수정</Button>
+              )}
               <Button onClick={() => deleteHandleClick(item._id)}>삭제</Button>
             </div>
           </EditContent>
@@ -106,6 +163,13 @@ const EditContent = styled.div`
   justify-content: space-between;
   border-bottom: 1px solid #e0e0e0;
   width: 82rem;
+  input {
+    margin-left: 3.8rem;
+    border: 0;
+    outline: none;
+    font-weight: 500;
+    font-size: 2.2rem;
+  }
   p {
     margin-left: 4rem;
     font-size: 2.2rem;
