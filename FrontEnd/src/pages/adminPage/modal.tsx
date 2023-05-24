1e9f5242230d9246315feaf5f384339fab4fe05d
Minyoung Lee <zhal7779@gmail.com>
2023-05-24 14:04:40 +0900

    [add] 관리자페이지 - 게임기록관리 모달창 CSS 완료

--- FrontEnd/src/pages/adminPage/modal.tsx
+++ FrontEnd/src/pages/adminPage/modal.tsx
@@ -48,30 +48,46 @@ export default Modal;
 const ModalContainer = styled.div`
   position: fixed;
   z-index: 2;
-  width: 40rem;
-  height: 50rem;
-  background: #ffffff;
+  width: 30rem;
+  height: 40rem;
+  background: #c0c0c0;
   border-radius: 5px;
+  border: 2px solid #4a4d4d;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.3);
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
-const ModalContent = styled.div``;
+const ModalContent = styled.div`
+  margin: 2.4rem 0.8rem;
+  width: 28rem;
+  height: 30rem;
+  background: #ffffff;
+  border-radius: 2px;
+`;
+
 const Header = styled.div`
   position: relative;
   width: 100%;
-  height: 5rem;
+  height: 4rem;
   background: #000080;
-  border-radius: 5px 5px 0 0;
 `;
+
 const Close = styled.span`
   position: absolute;
   top: 1rem;
   right: 1rem;
   width: 2rem;
   height: 2rem;
-  background: #ffffff;
-  font-size: 1.5rem;
+  background: #c0c0c0;
+  border-radius: 1px;
+  font-size: 2.2rem;
+  font-weight: 600;
+  color: #808080;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.3);
 `;
