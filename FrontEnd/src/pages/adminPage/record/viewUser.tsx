213e443cc327c66a94be04f4f86c797fe9a98ef1
Minyoung Lee <zhal7779@gmail.com>
2023-05-24 23:42:48 +0900

    [add] 관리자 페이지- 검색창 구현

--- FrontEnd/src/pages/adminPage/record/viewUser.tsx
+++ FrontEnd/src/pages/adminPage/record/viewUser.tsx
@@ -1,8 +1,49 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-
+import { ReactComponent as Search } from '../../../style/icons/icons8-google.svg';
 const ViewUser = () => {
-  return <h1>유저별 보기</h1>;
+  const [input, setInput] = useState<string>('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  const handleClick = () => {
+    console.log(input);
+    setInput('');
+  };
+  return (
+    <Content>
+      <SearchInput
+        onChange={handleChange}
+        placeholder="조회할 유저의 닉네임을 입력해주세요."
+      ></SearchInput>
+      <Button onClick={handleClick}>
+        <Search />
+      </Button>
+    </Content>
+  );
 };
 
 export default ViewUser;
+
+const Content = styled.div`
+  display: flex;
+`;
+const SearchInput = styled.input`
+  width: 60rem;
+  height: 5rem;
+  padding: 1rem 2rem;
+  margin-right: 1rem;
+  border-radius: 15px;
+  background-color: rgb(233, 233, 233);
+  border: 0;
+  border-radius: 10px;
+  outline: none;
+`;
+const Button = styled.div`
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  &:active {
+    transform: scale(1.1);
+  }
+`;
