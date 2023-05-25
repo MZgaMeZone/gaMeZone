b797d2208f3c56864a1f75d7ed6fbe92f772159d
Minyoung Lee <zhal7779@gmail.com>
2023-05-26 06:20:53 +0900

    [style] 관리자 페이지 - 회원관리 레이아웃 구현

--- FrontEnd/src/pages/adminPage/user/userList.tsx
+++ FrontEnd/src/pages/adminPage/user/userList.tsx
@@ -8,33 +8,36 @@ const UserList = () => {
     { nickname: '^O^/', email: '1234567@gmail.com' },
     { nickname: '^O^/', email: '1234567@gmail.com' },
     { nickname: '^O^/', email: '1234567@gmail.com' },
+    { nickname: '*^O^/', email: '1234567@gmail.com' },
   ];
 
   return (
     <Container>
       <Title>회원 리스트</Title>
-      <NavBar>
-        <p>프로필</p>
-        <p>닉네임</p>
-        <p>이메일</p>
-      </NavBar>
-      {dermyData.map((item, index) => (
-        <Content key={index}>
-          <ImageContent>
-            <GameImage
-              // src={item.gameImageUrl}
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7aRfWK5rqENOh5_8z0VK5FEKlGLcEfi-CLg&usqp=CAU"
-              alt="게임 아이콘"
-            />
-          </ImageContent>
-          <p>{item.nickname}</p>
-          <p>{item.email}</p>
-          <div>
-            <Button>수정</Button>
-            <Button>탈퇴</Button>
-          </div>
-        </Content>
-      ))}
+      <Main>
+        <NavBar>
+          <p>프로필</p>
+          <p>닉네임</p>
+          <p>이메일</p>
+        </NavBar>
+        {dermyData.map((item, index) => (
+          <Content key={index}>
+            <ImageContent>
+              <GameImage
+                // src={item.gameImageUrl}
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7aRfWK5rqENOh5_8z0VK5FEKlGLcEfi-CLg&usqp=CAU"
+                alt="게임 아이콘"
+              />
+            </ImageContent>
+            <NameText>{item.nickname}</NameText>
+            <EmailText>{item.email}</EmailText>
+            <ButtonDiv>
+              <Button>수정</Button>
+              <Button>탈퇴</Button>
+            </ButtonDiv>
+          </Content>
+        ))}
+      </Main>
     </Container>
   );
 };
@@ -42,21 +45,32 @@ const UserList = () => {
 export default UserList;
 
 const Container = styled.div`
+  position: relative;
   width: 100%;
   height: auto;
 `;
+
 const Title = styled.p`
   margin: 3rem 0 0 5rem;
   font-size: 2.4rem;
   margin-bottom: 4rem;
   font-weight: 600;
 `;
+const Main = styled.div`
+  position: relative;
+  width: 112rem;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+`;
 const NavBar = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  margin: 3rem 0 2rem 0;
-  padding: 1.6rem 50rem 1.6rem 8rem;
+  margin: 3rem 0 0 0;
+  padding: 1.6rem 54rem 1.6rem 7rem;
   font-size: 1.8rem;
   font-weight: 600;
   color: #242424;
@@ -67,10 +81,23 @@ const NavBar = styled.div`
 `;
 const Content = styled.div`
   display: flex;
+  flex-direction: row;
+  position: relative;
+  align-items: center;
   border-bottom: 2px solid #e0e0e0;
+  p {
+    position: absolute;
+    font-size: 2rem;
+  }
+`;
+const NameText = styled.p`
+  left: 28rem;
+`;
+const EmailText = styled.p`
+  left: 48rem;
 `;
 const ImageContent = styled.div`
-  margin: 1rem;
+  margin: 1rem 0 1rem 5rem;
   width: 10rem;
   height: 10rem;
   border: 3px solid #242424;
@@ -79,13 +106,16 @@ const ImageContent = styled.div`
 `;
 const GameImage = styled.img`
   border-radius: 7px;
-  width: 8rem;
+  width: 10rem;
 `;
-
+const ButtonDiv = styled.div`
+  position: absolute;
+  right: 5rem;
+`;
 const Button = styled.button`
   margin-right: 2rem;
-  width: 8rem;
-  height: 4rem;
+  width: 9rem;
+  height: 4.4rem;
   background: #000080;
   border-radius: 10px;
   color: #ffffff;
